21f28ffbdbf134b635feb950d500410c4ae494c4
ziya <644735414@qq.com>
2022-07-22 05:32:47 -0700

    突破512字节

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 
 int main() {
     char* boot_filepath = "/home/ziya/CLionProjects/ziya-os-research/build/boot/boot.o";
+    char* setup_filepath = "/home/ziya/CLionProjects/ziya-os-research/build/boot/setup.o";
 
     Floppy* floppy = create_floppy();
 
     Fileinfo* boot_fileinfo = read_file(boot_filepath);
     write_bootloader(floppy, boot_fileinfo);
 
-    create_image("/home/ziya/CLionProjects/ziya-os-research/a.img", floppy);
+    Fileinfo* setup_fileinfo = read_file(setup_filepath);
+    write_floppy_fileinfo(floppy, setup_fileinfo, 0, 0, 2);
 
-    return 0;
+    create_image("/home/ziya/CLionProjects/ziya-os-research/a.img", floppy);
 }
