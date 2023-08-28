f7c98ee0c3d4f8058327a5544aaaca8515366dfb
ty <752448099@qq.com>
2023-08-29 01:05:11 +0800

    gdt表少了f, 使用c实现

---
+++ oskernel/include/linux/traps.h
@@ -0,0 +1,12 @@
+//
+// Created by ziya on 22-7-5.
+//
+
+#ifndef ZIYA_OSKERNEL_RESEARCH_TRAPS_H
+#define ZIYA_OSKERNEL_RESEARCH_TRAPS_H
+
+#include "head.h"
+
+void gdt_init();
+
+#endif //ZIYA_OSKERNEL_RESEARCH_TRAPS_H
