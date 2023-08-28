e2ff1481a9bb5f4cf3ed784cdfb0f584fa0908cb
ty <752448099@qq.com>
2023-08-28 22:20:03 +0800

    创建0号进程、idle进程

--- oskernel/kernel/sched.c
+++ oskernel/kernel/sched.c
@@ -7,10 +7,36 @@
 #include "../include/linux/kernel.h"
 #include "../include/asm/system.h"
 
+extern void switch_task(task_t* task);
+extern void switch_idle_task(task_t* task);
+
 extern task_t* tasks[NR_TASKS];
 
 task_t* current = NULL;
 
+task_t* find_ready_task() {
+    task_t* next = NULL;
+
+    for (int i = 1; i < NR_TASKS; ++i) {
+        task_t* task = tasks[i];
+
+        if (NULL == task) continue;
+        if (TASK_READY != task->state) continue;
+
+        next = task;
+    }
+
+    return next;
+}
+
 void sched() {
-    current = tasks[0];
+    task_t* next = find_ready_task();
+
+    if (NULL == next) {
+        current = tasks[0];
+
+        switch_idle_task(tasks[0]);
+
+        return;
+    }
 }
